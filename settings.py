c6b373e5192bc7e11cfb397b5dd08dfca2676c42
Forrest Beck <forrest.beck@da.org>
2021-09-15 12:23:01 -0400

    updated - now deletes classes in jamf that are no longer in vc

--- settings.py
+++ settings.py
@@ -1,8 +1,16 @@
-# List of Veracross divisions to skip
-skip_class_division_level = ['Upper School',
-                             'All School',
-                             'Preschool']
-# List of Veracross class types to skip
-skip_class_type = ['Other Program',
-                   'Extended Care',
-                   'Athletic Program']
+skip_class_division_level = [
+    'Upper School',
+    'All School',
+    'Preschool',
+]
+skip_class_type = [
+    'Other Program',
+    'Extended Care',
+    'Athletic Program'
+]
+
+# if we can't find a previous update date, this will be used
+default_date_in_past = "2018-01-01"
+
+# Logging
+logFile = "logVeracrossJSS.log"
